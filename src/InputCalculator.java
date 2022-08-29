dc4260b61ebcd05f4bcca4511a16ac6625703594
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-29 10:58:49 +0530

    IT IS UDEMY EXERCISE.

--- src/InputCalculator.java
+++ src/InputCalculator.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class InputCalculator {
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage() {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+
+
+        while (true) {
+            boolean a = input.hasNextInt();
+            if (a) {
+
+                int abc = input.nextInt();
+                sum += abc;
+                count++;
+            } else {
+                break;
+            }
+        }
+        System.out.println("SUM = " + sum + " AVG = " + Math.round((double) sum/count));
+}
+
 }
